3c9a83f5e69a1002702e743b00ce2520291a6ee3
Shivasai Mamidala <105928178+shivasai789@users.noreply.github.com>
2025-06-03 19:37:01 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 
 const app = express();
-const FLASK_BACKEND = process.env.BACKEND_URL || 'http://127.0.0.1:5000'; // Flask backend URL
+const FLASK_BACKEND = 'http://35.154.165.19:5000'; // Flask backend URL
 
 // Configuration
 app.set('view engine', 'ejs');
